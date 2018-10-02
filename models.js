14fc49e05ab88e3248926e4933668dc68d0008c6
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-10-02 23:50:34 +0000

    Challenge complete

--- models.js
+++ models.js
@@ -12,6 +12,21 @@ const authorSchema = mongoose.Schema ({
   }
 });
 
+// Validation middleware to prevent duplicate userNames
+  // Source: https://stackoverflow.com/questions/16882938/how-to-check-if-that-data-already-exist-in-the-database-during-update-mongoose
+  
+authorSchema.pre('save', function(next) {
+  let self = this;
+  Author.find({userName: self.userName}, function(err, docs) {
+    if (!docs.length) {
+      next();
+    } else {
+      console.log(`User \`${self.userName}\` already exists`);
+      next(new Error('User already exists!'));
+    }
+  });
+});
+
 // Define comment schema
 const commentSchema = mongoose.Schema ({
   content: String
@@ -43,29 +58,44 @@ const blogPostSchema = mongoose.Schema({
   blogPostSchema.virtual("authorName").get(function() {
     return `${this.author.firstName} ${this.author.lastName}`.trim();
   });
+  
+  authorSchema.virtual('authorName').get(function() {
+    return `${this.firstName} ${this.lastName}`.trim();
+  });
 
 // Instance methods
-  // .serialize()
-  blogPostSchema.methods.serialize = function() {
-    return {
-      id: this._id,
-      title: this.title,
-      content: this.content,
-      authorName: this.authorName,
-      comments: this.comments,
-      created: this.created
+  // BLOG POST
+    // .serialize()
+    blogPostSchema.methods.serialize = function() {
+      return {
+        id: this._id,
+        title: this.title,
+        content: this.content,
+        authorName: this.authorName,
+        comments: this.comments,
+        created: this.created
+      };
     };
-  };
+    
+    // .serializeNoComments()
+    blogPostSchema.methods.serializeNoComments = function() {
+      return {
+        title: this.title,
+        content: this.content,
+        authorName: this.authorName,
+        created: this.created
+      };
+    };
   
-  // .serializeNoComments()
-  blogPostSchema.methods.serializeNoComments = function() {
-    return {
-      title: this.title,
-      content: this.content,
-      authorName: this.authorName,
-      created: this.created
+  // AUTHOR
+  // .serialize()
+    authorSchema.methods.serialize = function() {
+      return {
+        _id: this._id,
+        name: this.authorName,
+        userName: this.userName
+      };
     };
-  }
   
 
   
--- server.js
+++ server.js
@@ -113,15 +113,105 @@ app.use(express.json());
     .catch(err => res.status(500).json({message: 'Internal server error'}));
   });
 
-  // CREATE author
+  // GET authors
+  app.get('/authors', (req, res)=> {
+    Author
+      .find()
+      .limit(10)
+      .then(author => {
+        res.json({
+          authors: author.map(
+            (author) => author.serialize())
+        });
+      })
+      .catch(
+        err => {
+          console.error(err);
+          res.status(500).json({message: 'Internal server error'});
+        });
+  });
   
+  // GET authors/:id
+  app.get('/authors/:id', (req, res) => {
+    Author
+      .findById(req.params.id)
+      .then(author => res.json(author.serialize()))
+      .catch( err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+      });
+  });
+
+  // GET authors/:userName
+  app.get('/authors/userName/:userName', (req, res) => {
+    Author
+      .findOne({userName: req.params.userName})
+      .then(author => res.json(author.serialize()))
+      .catch(err => {
+        res.status(500).json({message: 'Internal server error'});
+      });
+  });
+
+  // CREATE author
+  app.post('/authors', (req, res) => {
+    // Verify required fields
+    const requiredFields = ['firstName', 'lastName', 'userName'];
+    for (let i = 0; i < requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if(!(field in req.body)) {
+        const message = `Missing\`${field}\` in request body`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+  
+    Author.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName
+    })
+      .then(author => res.status(201).json(author.serialize()))
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+      });
+  });
   
   // UPDATE author
+  app.put('/authors/:id', (req,res) => {
+    // Confirm params & body id fields match
+    if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+      const message = 
+        `Request path id (${req.params.id}) and request body id ` +
+        `(${req.body.id}) must match`;
+      console.error(message);
+      return res.status(400).json({message: message});
+    }
+    
+    // Only update accepted fields
+    const toUpdate = {};
+    const updateableFields = ['firstName', 'lastName', 'userName'];
+    
+    // Set fields to update
+    updateableFields.forEach(field => {
+      if (field in req.body) {
+        toUpdate[field] = req.body[field];
+      }
+    });
+    
+    // Update the author
+    Author
+      .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+      .then(author => res.status(200).json(author.serialize()))
+      .catch(err => res.status(500).json({message: 'Internal server error'}));
+  });
   
   // DELETE author
-
-
-
+  app.delete('/authors/:id', (req, res) => {
+    Author.findByIdAndRemove(req.params.id)
+    .then(author => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+  });
 
   // Catch all for non-existent endpoints
   app.use('*', (req, res) => {
